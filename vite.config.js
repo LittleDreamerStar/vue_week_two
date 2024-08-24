aff93a6f4b2628179d74b732a3af1ea9a55f88ad
Natto Tsai <fracture_star@hotmail.com>
2024-08-24 13:08:34 +0800

    update two week

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/vue_week_two/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
